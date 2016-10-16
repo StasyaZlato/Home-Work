c102daad49573c8c58969638edf471bed9c7a8cf
StasyaZlato <stasyaz16kursk@gmail.com>
2016-10-16 16:24:57 +0300

    hw3
    hw3

---
+++ HW3/programming_HW1_variant7.py
@@ -0,0 +1,11 @@
+a = int (input ())
+b = int (input ())
+c = int (input ())
+if a / b == c:
+    print (a, "разделить на", b, "равно", c)
+else:
+    print (a, "разделить на", b, "не равно", c)
+if a ** b == c:
+    print (a, "в степени", b, "равно", c)
+else:
+    print (a, "в степени", b, "не равно", c)
