f7b42350f65cbbbc90aa37a042e0209ca1fefaa6
Anastasia <stasyaz16kursk@gmail.com>
2016-12-06 16:32:18 +0300

    test
    test

--- programming_test.py
+++ programming_test.py
@@ -23,6 +23,23 @@ with open ('freq.txt', 'r', encoding = 'utf-8') as f:
         elem = float (elem)
         ipm_sum += elem
     print (ipm_sum)
+
+
+#Задание3. Спрашиваем слова, пока пользователь не введет пустое слово.
+#Выводим для каждого морф. информацию и ipm
+
+with open ('freq.txt', 'r', encoding = 'utf-8') as f:
+    lines = f.readlines ()
+    word = input ()
+    while word:
+        for line in lines:
+            line = line.split()
+            if word in line:
+                print ('Морфологическая информация: ' + ' '.join (line[2:-2]))
+                print ('IPM = ' + line[-1])
+        word = input ()
+
+        
         
             
             
